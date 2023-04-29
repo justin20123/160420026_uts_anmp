a83b4aa8fed3fe6b8c2cd7d2d834ce470c5fa5d9
justin20123 <114899999+justin20123@users.noreply.github.com>
2023-04-29 11:39:14 +0700

    initial commit

---
+++ app/src/main/java/com/example/a160420026_uts_anmp/view/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.a160420026_uts_anmp.view
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+}
