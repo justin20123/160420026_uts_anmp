20303b7be10ef8d2a45e63f57ae82e17bc8a2444
justin20123 <114899999+justin20123@users.noreply.github.com>
2023-05-02 18:39:59 +0700

    m

--- app/src/main/java/com/example/a160420026_uts_anmp/view/MainActivity.kt
+++ app/src/main/java/com/example/a160420026_uts_anmp/view/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.a160420026_uts_anmp.view
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.lifecycle.ViewModelProvider.NewInstanceFactory.Companion.instance
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -10,6 +13,8 @@ import androidx.navigation.ui.setupActionBarWithNavController
 
 
 class MainActivity : AppCompatActivity() {
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
